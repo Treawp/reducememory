aeadd366a9c347196fdabbbdd668749acc31c477
Treawp <egahjka@gmail.com>
2026-03-12 05:33:36 +0700

    Update WorldRendererMixin.java

--- src/main/java/dev/gazcc/reducememory/mixin/WorldRendererMixin.java
+++ src/main/java/dev/gazcc/reducememory/mixin/WorldRendererMixin.java
@@ -1,11 +1,6 @@
-package dev.gazcc.reducememory.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.class_761;
 
-@Mixin(class_761.class)
+@Mixin(targets = "net.minecraft.client.renderer.LevelRenderer")
 public class WorldRendererMixin {
 }
