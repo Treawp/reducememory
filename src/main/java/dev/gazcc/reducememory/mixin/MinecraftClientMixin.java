57540af94eccaaf90e747c5e53443b9884de7669
Treawp <egahjka@gmail.com>
2026-03-12 04:57:05 +0700

    Update MinecraftClientMixin.java

--- src/main/java/dev/gazcc/reducememory/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/gazcc/reducememory/mixin/MinecraftClientMixin.java
@@ -1,21 +1,5 @@
 package dev.gazcc.reducememory.mixin;
-
-import dev.gazcc.reducememory.ReduceMemoryMod;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public abstract class MinecraftClientMixin {
-    @Shadow public ClientWorld world;
-
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;Z)V", at = @At("TAIL"))
-    private void reducememory$onDisconnect(CallbackInfo ci) {
-        System.gc();
-        System.gc();
-    }
-}
+@Mixin(targets = "net.minecraft.client.Minecraft")
+public class MinecraftClientMixin {}
