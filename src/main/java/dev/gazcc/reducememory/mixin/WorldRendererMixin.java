9fb20a19f42fa7f312ff5d92fba171dc203fd260
Treawp <egahjka@gmail.com>
2026-03-12 05:30:12 +0700

    Update WorldRendererMixin.java

--- src/main/java/dev/gazcc/reducememory/mixin/WorldRendererMixin.java
+++ src/main/java/dev/gazcc/reducememory/mixin/WorldRendererMixin.java
@@ -1,18 +1,11 @@
 package dev.gazcc.reducememory.mixin;
 
-import net.minecraft.client.renderer.LevelRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.class_761;
 
-@Mixin(LevelRenderer.class)
+@Mixin(class_761.class)
 public class WorldRendererMixin {
-    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true, require = 0)
-    private static int limitRenderDistance(int renderDistance) {
-        long maxMem = Runtime.getRuntime().maxMemory() / (1024 * 1024);
-        if (maxMem < 512) return Math.min(renderDistance, 4);
-        if (maxMem < 1024) return Math.min(renderDistance, 6);
-        if (maxMem < 2048) return Math.min(renderDistance, 8);
-        return renderDistance;
-    }
 }
