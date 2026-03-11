fb0d849635eda9f27cbb897db4654681335c211b
Treawp <egahjka@gmail.com>
2026-03-12 04:25:38 +0700

    Create ParticleManagerMixin.java

---
+++ src/main/java/dev/gazcc/reducememory/mixin/ParticleManagerMixin.java
@@ -0,0 +1,8 @@
+package dev.gazcc.reducememory.mixin;
+
+import net.minecraft.client.particle.ParticleManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ParticleManager.class)
+public class ParticleManagerMixin {
+}
