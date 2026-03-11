237ccfcacc3c78529410e6d25d173df041682c03
Treawp <egahjka@gmail.com>
2026-03-12 04:56:39 +0700

    Update ClientChunkManagerMixin.java

--- src/main/java/dev/gazcc/reducememory/mixin/ClientChunkManagerMixin.java
+++ src/main/java/dev/gazcc/reducememory/mixin/ClientChunkManagerMixin.java
@@ -1,19 +1,5 @@
 package dev.gazcc.reducememory.mixin;
-
-import dev.gazcc.reducememory.ReduceMemoryMod;
-import net.minecraft.client.world.ClientChunkManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(ClientChunkManager.class)
-public class ClientChunkManagerMixin {
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientChunkManager$ClientChunkMap;<init>(I)V"), index = 0, require = 0)
-    private static int reducememory$reduceChunkCacheRadius(int original) {
-        long maxMem = Runtime.getRuntime().maxMemory() / (1024 * 1024);
-        if (maxMem < 512) return Math.max(2, original - 3);
-        if (maxMem < 1024) return Math.max(3, original - 2);
-        if (maxMem < 2048) return Math.max(4, original - 1);
-        return original;
-    }
-}
+@Mixin(targets = "net.minecraft.client.multiplayer.ClientChunkCache")
+public class ClientChunkManagerMixin {}
