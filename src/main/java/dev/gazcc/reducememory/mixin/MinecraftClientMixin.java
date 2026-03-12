9e5882fdb2356b38b57c00d90709060b41fc00e6
Treawp <egahjka@gmail.com>
2026-03-12 08:28:31 +0700

    Update MinecraftClientMixin.java

--- src/main/java/dev/gazcc/reducememory/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/gazcc/reducememory/mixin/MinecraftClientMixin.java
@@ -1,26 +1,22 @@
 package dev.gazcc.reducememory.mixin;
 
 import dev.gazcc.reducememory.ReduceMemoryMod;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V",
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;Z)V",
             at = @At("TAIL"), require = 0)
     private void onDisconnect(CallbackInfo ci) {
-        ReduceMemoryMod.LOGGER.info("[ReduceMemory] Disconnect - forcing GC");
-        System.gc();
-        System.gc();
-    }
-
-    @Inject(method = "clearLevel(Lnet/minecraft/client/gui/screens/Screen;)V",
-            at = @At("TAIL"), require = 0)
-    private void onClearLevel(CallbackInfo ci) {
-        System.gc();
+        if (ReduceMemoryMod.getConfig().gcOnDisconnect) {
+            ReduceMemoryMod.LOGGER.info("[ReduceMemory] Disconnect GC triggered");
+            System.gc();
+            System.gc();
+        }
     }
 }
