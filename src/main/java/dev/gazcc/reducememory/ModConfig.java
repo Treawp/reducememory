ddfd2ac2c44c6c5a24a653317b396c3ac8d6e5e1
Treawp <egahjka@gmail.com>
2026-03-12 08:55:45 +0700

    Update ModConfig.java

--- src/main/java/dev/gazcc/reducememory/ModConfig.java
+++ src/main/java/dev/gazcc/reducememory/ModConfig.java
@@ -4,25 +4,28 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "reducememory")
 public class ModConfig implements ConfigData {
 
-    @Comment("Aktifkan auto GC")
     public boolean enableAutoGC = true;
 
-    @Comment("Interval GC dalam detik (1-60)")
     @ConfigEntry.BoundedDiscrete(min = 1, max = 60)
     public int gcIntervalSeconds = 10;
 
-    @Comment("Threshold memory untuk trigger GC (50-95%)")
     @ConfigEntry.BoundedDiscrete(min = 50, max = 95)
     public int gcThresholdPercent = 80;
 
-    @Comment("Aktifkan GC saat disconnect world")
     public boolean gcOnDisconnect = true;
 
-    @Comment("Tampilkan info memory di chat saat GC")
     public boolean showGCNotification = false;
+
+    public boolean reduceCpuLoad = true;
+
+    public boolean reduceGpuLoad = true;
+
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 60)
+    public int maxFpsOnBackground = 10;
+
+    public boolean entityCulling = true;
 }
