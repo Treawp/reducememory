04e99fcc032cade340fae19bd31a87202d56480d
Treawp <egahjka@gmail.com>
2026-03-14 10:55:26 +0700

    Update MemoryHudRenderer.java

--- src/main/java/dev/gazcc/reducememory/MemoryHudRenderer.java
+++ src/main/java/dev/gazcc/reducememory/MemoryHudRenderer.java
@@ -7,9 +7,6 @@ import net.minecraft.client.DeltaTracker;
 
 public class MemoryHudRenderer {
 
-    private static final int W = 62;
-    private static final int H = 48;
-
     public static void register() {
         HudRenderCallback.EVENT.register(MemoryHudRenderer::render);
     }
@@ -23,20 +20,19 @@ public class MemoryHudRenderer {
         Runtime r = Runtime.getRuntime();
         long max = r.maxMemory() / (1024 * 1024);
         long used = (r.totalMemory() - r.freeMemory()) / (1024 * 1024);
-        int memPct = (int)((double) used / max * 100);
+        int mem = (int)((double) used / max * 100);
         int fps = mc.getFps();
 
         int x = cfg.hudX;
         int y = cfg.hudY;
 
-        ctx.fill(x, y, x + W, y + H, 0xBB000000);
+        int mCol = mem > 85 ? 0xFFFF4444 : mem > 65 ? 0xFFFFAA00 : 0xFF55FF55;
+        int fCol = fps < 15 ? 0xFFFF4444 : fps < 30 ? 0xFFFFAA00 : 0xFF55FF55;
 
-        int mCol = memPct > 85 ? 0xFF4444 : memPct > 65 ? 0xFFAA00 : 0x55FF55;
-        int fCol = fps < 15 ? 0xFF4444 : fps < 30 ? 0xFFAA00 : 0x55FF55;
-
-        ctx.drawString(mc.font, "M:" + memPct + "%", x + 3, y + 4, mCol);
-        ctx.drawString(mc.font, "F:" + fps, x + 3, y + 14, fCol);
-        ctx.drawString(mc.font, "C:N/A", x + 3, y + 24, 0xAAAAAA);
-        ctx.drawString(mc.font, "G:N/A", x + 3, y + 34, 0xAAAAAA);
+        ctx.fill(x, y, x + 65, y + 46, 0xBB000000);
+        ctx.drawString(mc.font, "M:" + mem + "%", x + 3, y + 4, mCol, true);
+        ctx.drawString(mc.font, "F:" + fps, x + 3, y + 14, fCol, true);
+        ctx.drawString(mc.font, "C:N/A", x + 3, y + 24, 0xFFAAAAAA, true);
+        ctx.drawString(mc.font, "G:N/A", x + 3, y + 34, 0xFFAAAAAA, true);
     }
-    }
+}
