a9b1933a07414ec5efd95f473973e2d1b4ea9edf
Treawp <egahjka@gmail.com>
2026-03-14 09:30:18 +0700

    Update ModConfig.java

--- src/main/java/dev/gazcc/reducememory/ModConfig.java
+++ src/main/java/dev/gazcc/reducememory/ModConfig.java
@@ -16,28 +16,39 @@ public class ModConfig implements ConfigData {
     public int gcThresholdPercent = 80;
 
     public boolean gcOnDisconnect = true;
-
     public boolean showGCNotification = false;
-
     public boolean reduceCpuLoad = true;
-
     public boolean reduceGpuLoad = true;
 
     @ConfigEntry.BoundedDiscrete(min = 1, max = 60)
     public int maxFpsOnBackground = 10;
 
     public boolean entityCulling = true;
-
-    // Anti lag features
     public boolean improveChunkLoading = true;
 
     @ConfigEntry.BoundedDiscrete(min = 2, max = 16)
     public int maxChunkUpdatesPerTick = 4;
 
     public boolean reduceParticles = true;
-
     public boolean gcOnLowMemory = true;
 
     @ConfigEntry.BoundedDiscrete(min = 10, max = 90)
     public int lowMemoryThreshold = 85;
-}
+
+    // Memory HUD
+    public boolean showMemoryHud = true;
+    public boolean showMemoryBar = true;
+
+    // Chunk Unload Boost
+    public boolean chunkUnloadBoost = true;
+
+    @ConfigEntry.BoundedDiscrete(min = 50, max = 95)
+    public int chunkUnloadThreshold = 75;
+
+    // Memory Preset
+    public MemoryPreset memoryPreset = MemoryPreset.MEDIUM;
+
+    public enum MemoryPreset {
+        LOW, MEDIUM, HIGH
+    }
+    }
